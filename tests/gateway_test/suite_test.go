d439295ae27bb92052393e769ef66b8c1caf799c
Arturs Jefimovs <arturefimov@gmail.com>
2025-05-19 21:37:23 +0300

    feat: allow controller-runtime log level in tests to be set via LOG_LEVEL

--- tests/gateway_test/suite_test.go
+++ tests/gateway_test/suite_test.go
@@ -1,24 +1,29 @@
 package gateway_test
 
 import (
+	"net/http/httptest"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	v1 "k8s.io/api/rbac/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"net/http/httptest"
-	"os"
-	"path/filepath"
-	"testing"
 
 	"github.com/graphql-go/graphql"
 	"github.com/openmfp/golang-commons/logger"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
+	"go.uber.org/zap/zapcore"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	"sigs.k8s.io/controller-runtime/pkg/kcp"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
 	"github.com/openmfp/account-operator/api/v1alpha1"
 	appConfig "github.com/openmfp/kubernetes-graphql-gateway/common/config"
@@ -26,6 +31,25 @@ import (
 	"github.com/openmfp/kubernetes-graphql-gateway/gateway/resolver"
 	"github.com/openmfp/kubernetes-graphql-gateway/gateway/schema"
 )
+
+func TestMain(m *testing.M) {
+	var zapLevel zapcore.Level
+	level := strings.ToUpper(os.Getenv("LOG_LEVEL"))
+	switch level {
+	case "ERROR":
+		zapLevel = zapcore.ErrorLevel
+	case "WARN":
+		zapLevel = zapcore.WarnLevel
+	case "INFO":
+		zapLevel = zapcore.InfoLevel
+	case "DEBUG", "TRACE":
+		zapLevel = zapcore.DebugLevel
+	default:
+		zapLevel = zapcore.ErrorLevel
+	}
+	log.SetLogger(zap.New(zap.UseDevMode(false), zap.Level(zapLevel)))
+	os.Exit(m.Run())
+}
 
 type CommonTestSuite struct {
 	suite.Suite
